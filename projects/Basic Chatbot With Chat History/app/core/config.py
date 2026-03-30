0c532e2d9a42f3ee8a32841ab6bca71a16c77728
Pushp Hans <104348251+pushphans@users.noreply.github.com>
2026-03-30 10:34:21 +0530

    updated file encoding

--- projects/Basic Chatbot With Chat History/app/core/config.py
+++ projects/Basic Chatbot With Chat History/app/core/config.py
@@ -2,7 +2,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
-    OPENAI_API_KEY: str
+    GROQ_API_KEY: str
 
     config_model = SettingsConfigDict(
         env_file=".env", env_file_encoding="utf-8", extra="ignore", case_sensitive=False
